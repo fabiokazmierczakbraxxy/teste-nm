3d156aadd6bc9de46a46e7aef367a10f741925b3
admin <admin@BRXX-NOT1-01>
2020-11-26 21:20:31 -0300

    Usando metodo

--- src/br/com/kaz/MainClass.java
+++ src/br/com/kaz/MainClass.java
@@ -5,10 +5,10 @@ public class MainClass {
 	public static void main(String[] args) {
 
 		System.out.println("Main method");
-		
+		metodoDaniel();
 	}
 	
-	void metodoDaniel() {
+	static void metodoDaniel() {
 		String a = "Ahhhh";
 		String b = " sim, off course";
 		System.out.println("Metodo Fabio!!!" + a+b);
