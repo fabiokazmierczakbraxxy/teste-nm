1ba613ebb4e61b1b715e75fe36f01d340c4b1d45
admin <admin@BRXX-NOT1-01>
2020-11-26 21:45:04 -0300

    rtrtr

--- src/br/com/kaz/MainClass.java
+++ src/br/com/kaz/MainClass.java
@@ -10,7 +10,8 @@ public class MainClass {
 	
 	static void metodoDaniel() {
 		String a = "Ahhhh";
-		System.out.println("Metodo Fabio!!!" + a);
+		String c = " C";
+		System.out.println("Metodo Fabio!!!" + a + c);
 	}
 
 }
